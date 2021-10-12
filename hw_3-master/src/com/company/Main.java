c66651e7fd5379c207fd14bcaea6efb669cca2b1
User <rtataryn@gmail.com>
2021-10-12 18:08:35 +0300

    Initial commit

---
+++ hw_3-master/src/com/company/Main.java
@@ -0,0 +1,22 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Task 1");
+        Circle.Area();
+        System.out.println("--------------------------");
+        System.out.println("Task 2");
+        Avto.zavesty_avto();
+        Avto.nabraty_shvydkist();
+        Avto.stop();
+        String model;
+        double max_speed;
+        System.out.println("-------------------------");
+        System.out.println("Task 3");
+        Money.kalk();
+
+    }
+
+
+}
